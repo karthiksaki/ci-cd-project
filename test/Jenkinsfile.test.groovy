649b9074ee5effa97deb0ba85b60e2b70faa3fbb
karthiksaki <sakibanda.karthi@gmail.com>
2025-01-07 14:54:05 -0500

    including test cases

---
+++ test/Jenkinsfile.test.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Run Tests') {
+            steps {
+                script {
+                    def testResults = jenkinsPipelineUnit.runTests()
+                    junit testResults
+                }
+            }
+        }
+        stage('Report Results') {
+            steps {
+                echo 'Test results reported.'
+            }
+        }
+    }
+}
